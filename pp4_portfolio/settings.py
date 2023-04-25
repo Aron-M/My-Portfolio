3a7bfbd9446ad1d35cbe0d4c6a0ec0bf782711ab
Aron-M <aron.moelich88@gmail.com>
2023-04-25 07:17:00 +0000

    connecting ElephantSQL database with project, activating on Heroku

--- pp4_portfolio/settings.py
+++ pp4_portfolio/settings.py
@@ -19,6 +19,11 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+     import env
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -78,13 +83,16 @@ WSGI_APPLICATION = 'pp4_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
