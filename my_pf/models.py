ef3578c71a5d200d6e3b02808bb40017ff62a253
Aron-M <aron.moelich88@gmail.com>
2023-04-28 12:43:22 +0000

    defined PersonalDetails model and made migrations

--- my_pf/models.py
+++ my_pf/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class PersonalDetails(models.Model):
-    full_name = CharField
-    age = PositiveBigInteger
-    nationality = CharField
-    residency = CharField
-    languages = CharField
-    address = CharField
+    full_name = models.CharField(max_length=200, unique=False)
+    age = models.PositiveBigIntegerField
+    nationality = models.CharField(max_length=200, unique=False)
+    residency = models.CharField(max_length=200, unique=False)
+    languages = models.CharField(max_length=200, unique=False)
+    address = models.CharField(max_length=200, unique=False)
 
+    class Meta:
 
+        def __str__(self):
+            return self.title
+
+    
     
