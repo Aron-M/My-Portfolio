ffbd4d5e3f966face0484b05e586bb880c5a926f
Aron-M <aron.moelich88@gmail.com>
2023-04-28 12:32:43 +0000

    created superuser and created PersonalDetails Model

--- my_pf/models.py
+++ my_pf/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PersonalDetails(models.Model):
+    full_name = CharField
+    age = PositiveBigInteger
+    nationality = CharField
+    residency = CharField
+    languages = CharField
+    address = CharField
+
+
+    
