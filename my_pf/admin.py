37eb9229a7c4102f963320bd93c8051ae3ed7897
Aron-M <aron.moelich88@gmail.com>
2023-05-07 10:40:42 +0000

    created two models for skills, namely 'languages' and 'frameworks'. Installed Pillow in order to display images for each model

--- my_pf/admin.py
+++ my_pf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import PersonalDetails, Skills
+from .models import PersonalDetails, Skills, Language, Framework
+
 
 admin.site.register(PersonalDetails)
 admin.site.register(Skills)
+admin.site.register(Language)
+admin.site.register(Framework)
--- my_pf/models.py
+++ my_pf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 
@@ -23,6 +25,22 @@ class Skills(models.Model):
 
     def __str__(self):
             return self.languages
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='languages/', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
+
+    def __str__(self):
+        return self.name
+
+
+class Framework(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='frameworks/', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
+
+    def __str__(self):
+        return self.name
 
 
     
