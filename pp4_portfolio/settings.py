6ffcaf099eadb1509cf3bc04bec1fff902745221
Aron-M <aron.moelich88@gmail.com>
2023-05-20 09:44:33 +0000

    adding '8000-aronm-project4portfoli-e3zn0a2avrd.ws-eu97.gitpod.io' to ALLOWED HOSTS

--- pp4_portfolio/settings.py
+++ pp4_portfolio/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["aron-portfolio.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["aron-portfolio.herokuapp.com", "localhost", '8000-aronm-project4portfoli-e3zn0a2avrd.ws-eu97.gitpod.io']
 
 
 # Application definition
