5182e2a85c11ab10a9bf7ae6e1a552f7887e2831
Aron-M <aron.moelich88@gmail.com>
2023-05-20 21:21:32 +0000

    adding urls for all 'skills icons' from within django admin panel

--- my_pf/models.py
+++ my_pf/models.py
@@ -56,57 +56,8 @@ class Skills(models.Model):
     library_icon4 = models.URLField(max_length=100, blank=True, null=True)
     library_icon5 = models.URLField(max_length=100, blank=True, null=True)
     
-    def save(self, *args, **kwargs):
-        if self.language_name:
-            self.language_icon1 = get_devicon(self.language_name)
-            self.language_icon2 = get_devicon(self.language_name)
-            self.language_icon3 = get_devicon(self.language_name)
-            self.language_icon4 = get_devicon(self.language_name)
-            self.language_icon5 = get_devicon(self.language_name)
-        if self.framework_name:
-            self.framework_icon1 = get_devicon(self.framework_name)
-            self.framework_icon2 = get_devicon(self.framework_name)
-            self.framework_icon3 = get_devicon(self.framework_name)
-            self.framework_icon4 = get_devicon(self.framework_name)
-            self.framework_icon5 = get_devicon(self.framework_name)
-        if self.database_name:
-            self.database_icon1 = get_devicon(self.database_name)
-            self.database_icon2 = get_devicon(self.database_name)
-            self.database_icon3 = get_devicon(self.database_name)
-            self.database_icon4 = get_devicon(self.database_name)
-            self.database_icon5 = get_devicon(self.database_name)
-        if self.version_control_name:
-            self.version_control_icon1 = get_devicon(self.version_control_name)
-            self.version_control_icon2 = get_devicon(self.version_control_name)
-            self.version_control_icon3 = get_devicon(self.version_control_name)
-            self.version_control_icon4 = get_devicon(self.version_control_name)
-            self.version_control_icon5 = get_devicon(self.version_control_name)
-        if self.library_name:
-            self.library_icon1 = get_devicon(self.library_name)
-            self.library_icon2 = get_devicon(self.library_name)
-            self.library_icon3 = get_devicon(self.library_name)
-            self.library_icon4 = get_devicon(self.library_name)
-            self.library_icon5 = get_devicon(self.library_name)
-        super().save(*args, **kwargs)
-    
     def __str__(self):
         return self.language_name
-
-def get_devicon(name):
-    # Implement your logic to retrieve the icon URL from www.devicon.dev based on the name
-    # You can use web scraping, an API, or any other method to fetch the icon URL.
-    # Return the icon URL as a string.
-    # Example implementation:
-    # return f"https://www.devicon.dev/icons/{name}/{name}-original.svg"
-
-    # Placeholder implementation returning None
-    return (
-          
-           "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
-          
-          )
-
-
 
 
 
