f5fa3da032236b1017fe3f309ad79d66bdf8b070
Aron-M <aron.moelich88@gmail.com>
2023-06-18 10:59:20 +0000

    added new navbar for 'dashboard' page to include login, signup' and logout' capabilities using allauth

--- my_pf/forms.py
+++ my_pf/forms.py
@@ -12,12 +12,12 @@ class HeadingsForm(forms.ModelForm):
         model = Headings
         exclude = []  # Exclude all fields by default
         widgets = {
-            'big_header': forms.Textarea(attrs={'rows':3, 'cols': 70}),
-            'sub_header': forms.Textarea(attrs={'rows':3, 'cols': 70}),
-            'par1': forms.Textarea(attrs={'rows': 3, 'cols': 70}),
-            'par2': forms.Textarea(attrs={'rows': 3, 'cols': 70}),
-            'par3': forms.Textarea(attrs={'rows': 3, 'cols': 70}),
-            'par4': forms.Textarea(attrs={'rows': 3, 'cols': 70}),
+            'big_header': forms.Textarea(attrs={'rows':3, 'cols': 60}),
+            'sub_header': forms.Textarea(attrs={'rows':3, 'cols': 60}),
+            'par1': forms.Textarea(attrs={'rows': 3, 'cols': 60}),
+            'par2': forms.Textarea(attrs={'rows': 3, 'cols': 60}),
+            'par3': forms.Textarea(attrs={'rows': 3, 'cols': 60}),
+            'par4': forms.Textarea(attrs={'rows': 3, 'cols': 60}),
         }
 
     def __init__(self, *args, **kwargs):
