ef126e7ebf5982fbd6c4c37d8c2957b6587a5f96
Aron-M <aron.moelich88@gmail.com>
2023-06-20 10:00:37 +0000

    Fixed bug where modal was displaying when page is loaded

--- static/pp4_portfolio/script.js
+++ static/pp4_portfolio/script.js
@@ -1,5 +1,5 @@
 // Get the modal element
-let modal = document.getElementById("modal-box");
+let modalDiv = document.getElementById("modal-div");
 
 // Get the image and text elements inside the modal
 let modalImage = document.getElementById("modal-image");
@@ -8,17 +8,19 @@ let modalTitle = document.querySelector("#modal-box h2");
 
 // Function to open the modal and display the content with fade-in effect
 function openModal(imageSrc, textContent, titleContent) {
-  modal.style.display = "block";
-  modal.classList.add("fade-in");
+  modalDiv.style.display = "block";
+  modalDiv.classList.remove("fade-in"); // Remove the fade-in class
+  void modalDiv.offsetWidth; // Trigger reflow to restart the animation
+  modalDiv.classList.add("fade-in"); // Add the fade-in class again
   modalImage.src = imageSrc;
+  modalImage.classList.add("fade-in");
   modalText.innerText = textContent;
   modalTitle.innerText = titleContent;
 }
 
 // Function to close the modal
 function closeModal() {
-  modal.style.display = "none";
-  modal.classList.remove("fade-in");
+  modalDiv.style.display = "none";
 }
 
 let icons = document.querySelectorAll(".modal-image");
@@ -36,11 +38,10 @@ closeButton.addEventListener("click", closeModal);
 
 // Close the modal when clicking outside of it
 window.addEventListener("click", function (event) {
-  if (event.target === modal) {
+  if (event.target === modalDiv) {
     closeModal();
   }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  closeModal();
-});
+// Call the closeModal function on page load
+closeModal();
