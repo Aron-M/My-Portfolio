5c0eadfa2fd57081f867fca151b993dfb1f19e47
Aron-M <aron.moelich88@gmail.com>
2024-04-13 11:44:15 +0100

    readding profile header image

--- pp4_portfolio/settings.py
+++ pp4_portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ["aron-portfolio.herokuapp.com", "localhost", '8000-aronm-project4portfoli-w899pig2hdb.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ["aron-portfolio.herokuapp.com", "localhost", '8000-aronm-project4portfoli-w899pig2hdb.ws-eu101.gitpod.io', '127.0.0.1']
 
 
 # Application definition
